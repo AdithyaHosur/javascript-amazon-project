a67c68115bdba4c88d6c2d7b76450934480331bd
AdithyaHosur <adithyahosur@gmail.com>
2024-03-12 22:39:36 +0530

    Save data and generate HTML for delivery options

--- scripts/utils/money.js
+++ scripts/utils/money.js
@@ -1,3 +1,7 @@
 export function formatCurrency(priceCents){
     return (priceCents / 100).toFixed(2);
 }
+
+export default formatCurrency;//only one 
+// default export available per file to make the 
+// code cleaner
