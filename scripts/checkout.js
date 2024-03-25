bc63ad03c42844ca8b2414648dfe3e1132ff68f7
AdithyaHosur <adithyahosur@gmail.com>
2024-03-25 08:44:11 +0530

    Make delivery options interactive using data attributes and event listeners

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import {cart, removeFromCart, calculateCartQuantity, updateQuantity} from '../data/cart.js';
+import {cart, removeFromCart, calculateCartQuantity, updateQuantity, updateDeliveryOption} from '../data/cart.js';
 import {products} from '../data/products.js';
 import {formatCurrency} from './utils/money.js';//means current folder
 import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
@@ -114,7 +114,9 @@ function deliveryOptionsHTML(matchingProduct, cartItem){
 
         html += `
             </div>
-                <div class="delivery-option">
+                <div class="delivery-option js-delivery-option"
+                data-product-id="${matchingProduct.id}"
+                data-delivery-option-id="${deliveryOption.id}">
                     <input type="radio" 
                     ${isChecked ? 'checked' : ''}
                     class="delivery-option-input"
@@ -177,3 +179,11 @@ document.querySelectorAll('.js-update-link')
             calculateCartQuantity();
         });
     });
+
+    document.querySelectorAll('.js-delivery-option')
+        .forEach((element) => {
+            element.addEventListener('click', () => {
+                const {productId, deliveryOptionId} = element.dataset;
+                updateDeliveryOption(productId, deliveryOptionId);
+            })
+        });
